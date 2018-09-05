cdf430ba15d7cc5bbbb7ba7258c986f6b9c4377f
Brent Bauman <brentbauman86@gmail.com>
2018-09-05 22:04:12 +0000

    automatically backed up by learn

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,21 @@
 class Pet < ActiveRecord::Base
   belongs_to :owner
 end
+
+class CreateOwnersTable < ActiveRecord::Migration
+  def change
+    create_table :owners do |t|
+      t.string :name
+    end
+  end
+end
+
+
+class CreatePetsTable < ActiveRecord::Migration
+  def change
+    create_table :pets do |t|
+      t.string :name
+      t.integer :owner_id
+    end
+  end
+end
